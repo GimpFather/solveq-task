b7a0032ed53841705a6d8e2d747bc948758db9dc
Filip Gałczyk <filip.galczyk@waynet.pl>
2022-07-22 19:25:23 +0200

    misunderstanding with starting time

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import Timemeasure from "./components/Timemeasure";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [lastSeen, setLastSeen] = useState<number>(0);
+	const [presentTimestamp, setPresentTimestamp] = useState<number>(
+		Math.floor(+new Date() / 1000)
+	);
+	const [timestampDiff, setTimestampDiff] = useState("");
+
+	const inputHandler = (event: React.FormEvent<HTMLInputElement>) => {
+		let inputValue: number = event.currentTarget.valueAsNumber;
+		setLastSeen(inputValue);
+	};
+
+	const submitHandler = (event: React.FormEvent) => {
+		event.preventDefault();
+		console.log(lastSeen);
+		console.log(presentTimestamp);
+
+		const timestampDiff = Math.abs(lastSeen - presentTimestamp);
+		console.log(timestampDiff);
+
+		if (timestampDiff < 60) {
+			console.log("0 ... 60 seconds ago");
+		} else {
+			setTimestampDiff(timestampDiff / 60 + "minutes ago");
+		}
+	};
+
+	useEffect(() => {
+		setInterval(() => {
+			setPresentTimestamp(Math.floor(+new Date() / 1000));
+		}, 1000);
+	}, []);
+
+	return (
+		<div className="wrapper">
+			<h3>Hello! Type there some UNIX data!</h3>
+			<br></br>
+			<form onSubmit={submitHandler}>
+				<label>Date in unixtimestamp</label>
+				<br></br>
+				<input onChange={inputHandler} name="time" type="number" />
+				<button type="submit">Calculate</button>
+			</form>
+			<div>
+				<h3>Present timestamp: {presentTimestamp}</h3>
+				<h3>Your timestamp: {lastSeen}</h3>
+				<h3>{timestampDiff}</h3>
+			</div>
+		</div>
+	);
 }
 
 export default App;
